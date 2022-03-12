d21c39f67d40ae151d79a834d7099f5b5581d7e2
Andre Diniz <andreg.diniz97@hotmail.com>
2022-03-12 10:31:12 -0300

    Desafio concluído!

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 import { usersRoutes } from "./routes/users.routes";
 
@@ -7,5 +7,26 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      if (err.message === "Not Found") {
+        return response.status(404).json({
+          error: err.message,
+        });
+      }
+
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error.",
+    });
+  }
+);
 
 export { app };
